690fa6a226574acf13ad2d7b693498ed7695699f
Eric Anderson <eric@vector241.com>
2016-04-12 14:32:19 -0500

    Generating Lineups

--- src/RandomLineupGenerator/Program.cs
+++ src/RandomLineupGenerator/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RandomLineupGenerator.Services;
 
 namespace RandomLineupGenerator
 {
-    class Program
+    public class Program
     {
+        public static readonly string TeamFile = @"c:\var\teeball\team.json";
+        public static readonly string LineupsFile = @"c:\var\teeball\lineups.json";
+        public static readonly string CsvFile = @"c:\var\teeball\lineups.csv";
+
         static void Main(string[] args)
         {
+            new SeasonPopulator().Populate();
         }
     }
 }
